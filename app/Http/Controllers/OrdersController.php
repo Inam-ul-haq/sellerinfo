77e502966ac4280d72fba5e28e49e8a3b782c71a
Inam ulhaq <inamulhaq714@gmail.com>
2021-01-06 19:55:26 +0000

    change two file necessary3

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -175,14 +175,14 @@ class OrdersController extends Controller
               $result = DB::table('orders')->select('note')->where('amzid','=',$id)->first();
                 if($result!= false){
                       $status = '1';
-                          $response = Response::json(array(['status'=>'1','note'=>$result->note]));
+                          $response = Response::json(array(['status'=> 1,'note'=>$result->note]));
                         $response->header('Content-Type', 'application/json');
                         return $response;
                       // return Response::json_encode(array($array,$array1));
                 }else{
                          // $status = '0';
                   
-                     $response = Response::json(['status'=>'0']);
+                     $response = Response::json(['status'=> 0]);
                      $response->header('Content-Type', 'application/json');
                     return $response;
                 }
